5fd6782520362eeb65dd327d4e40e9a82f22bf12
cceer <bhurtelshishir@gmail.com>
2021-06-26 21:49:50 +0545

    All done now

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import About from './Components/About';
 
 import { toast } from 'react-toastify';
@@ -9,85 +9,111 @@ import Footer from './Components/Footer';
 import Header from './Components/Header';
 import Projects from './Components/Projects';
 import Specializations from './Components/Specializations';
+import loading from './images/loading.gif';
 
 toast.configure();
 
-export default function App() {
-  useEffect(() => {
-    const toggleActiveState = (e) => {
-      document
-        .querySelectorAll('.nav-link')
-        .forEach((el) => el.classList.remove('active'));
-
-      e.classList.add('active');
-      document.querySelector('.nav-collection').classList.remove('active');
-      document.querySelector('body').style.overflow = 'scroll';
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true,
     };
+  }
 
-    document.addEventListener('scroll', () => {
-      const Y = window.scrollY;
-      const W = window.scrollX;
-      let thresholdHeight;
+  componentDidMount() {
+    this.setState({ loading: false });
+    try {
+      const toggleActiveState = (e) => {
+        document
+          .querySelectorAll('.nav-link')
+          .forEach((el) => el.classList.remove('active'));
 
-      if (W <= 1440) {
-        thresholdHeight = 1650;
-      } else if (W < 1024) {
-        thresholdHeight = 1809;
-      } else if (W < 425) {
-        thresholdHeight = 2350;
-      }
-      if (Y > thresholdHeight) {
-        const skillBar = document.querySelectorAll('.skill_level');
-        skillBar.forEach((el) => {
-          if (!el.classList.contains('animation')) {
-            const skillPercentage = el.classList[el.classList.length - 1];
-            el.classList.add('animation');
-            el.classList.add(`animation${skillPercentage}`);
-          }
-        });
-      }
-
-      const home = document.querySelector('#homelink');
-      const about = document.querySelector('#aboutlink');
-      const contact = document.querySelector('#contactlink');
-
-      const e = document.querySelector('nav');
-
-      let toggleStyle = (e) => {
-        e.style.position = 'fixed';
-        e.style.background = '#2a363f';
-
-        e.style.zIndex = 100000;
-        e.style.left = '0';
-        e.style.top = '0';
-
-        e.style.marginRight = '20px';
-        e.style.paddingLeft = '20px';
-        e.style.justifyContent = 'space-around';
+        e.classList.add('active');
+        document.querySelector('.nav-collection').classList.remove('active');
+        document.querySelector('body').style.overflow = 'scroll';
       };
 
-      if (Y >= 0 && Y < 860) {
-        toggleActiveState(home);
-      } else if (Y > 863 && Y < 3812) {
-        toggleActiveState(about);
-      }
+      document.addEventListener('scroll', () => {
+        const Y = window.scrollY;
+        const W = window.scrollX;
+        let thresholdHeight;
 
-      if (Y >= 840) {
-        toggleStyle(e);
-      } else {
-        e.style = null;
-      }
-    });
-  }, []);
-  return (
-    <div>
-      <Header />
-      <Specializations />
-      <About />
-      <Projects />
-      <Counter />
-      <Contact />
-      <Footer />
-    </div>
-  );
+        if (W <= 1440) {
+          thresholdHeight = 1650;
+        } else if (W < 1024) {
+          thresholdHeight = 1809;
+        } else if (W < 425) {
+          thresholdHeight = 2350;
+        }
+        if (Y > thresholdHeight) {
+          const skillBar = document.querySelectorAll('.skill_level');
+          skillBar.forEach((el) => {
+            if (!el.classList.contains('animation')) {
+              const skillPercentage = el.classList[el.classList.length - 1];
+              el.classList.add('animation');
+              el.classList.add(`animation${skillPercentage}`);
+            }
+          });
+        }
+
+        const home = document.querySelector('#homelink');
+        const about = document.querySelector('#aboutlink');
+        const contact = document.querySelector('#contactlink');
+
+        const e = document.querySelector('nav');
+
+        let toggleStyle = (e) => {
+          e.style.position = 'fixed';
+          e.style.background = '#2a363f';
+
+          e.style.zIndex = 100000;
+          e.style.left = '0';
+          e.style.top = '0';
+
+          e.style.marginRight = '20px';
+          e.style.paddingLeft = '20px';
+          e.style.justifyContent = 'space-around';
+        };
+
+        if (Y >= 0 && Y < 860) {
+          toggleActiveState(home);
+        } else if (Y > 863 && Y < 3812) {
+          toggleActiveState(about);
+        }
+
+        if (Y >= 840) {
+          toggleStyle(e);
+        } else {
+          e.style = null;
+        }
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  render() {
+    if (this.state.loading) {
+      return (
+        <div class="loading_screen">
+          <img src={loading} alt="Loading" />
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <Header />
+          <Specializations />
+          <About />
+          <Projects />
+          <Counter />
+          <Contact />
+          <Footer />
+        </div>
+      );
+    }
+  }
 }
+
+export default App;
