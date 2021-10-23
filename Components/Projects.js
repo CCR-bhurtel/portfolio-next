577319a42452f1a5c066125cbac42eec457ef5ed
cceer <sisir.sital@gmail.com>
2021-10-23 23:01:17 +0545

    done now

--- Components/Projects.js
+++ Components/Projects.js
@@ -1,68 +1,68 @@
-import React, { useState } from 'react';
-import Card from './Card';
-import GlassCard from './GlassCard';
+import React, { useState } from "react";
+import Card from "./Card";
+import GlassCard from "./GlassCard";
 
 function Projects() {
   const [projects, setProjects] = useState([
     {
-      projectName: 'Fanorona Game',
+      projectName: "Fanorona Game",
       description:
-        'Fanorona is the  multiplayer strategy game originated from Madagascar. The online version of the game is built in React.js, Redux.js, node.js and socket.io.',
-      image: 'fanorona.png',
-      link: 'https://fanorona-game.netlify.app',
+        "Fanorona is the  multiplayer strategy game originated from Madagascar. The online version of the game is built in React.js, Redux.js, node.js and socket.io.",
+      image: "fanorona.png",
+      link: "https://fanorona-game.netlify.app",
     },
     {
-      projectName: 'ROLACON',
+      projectName: "ROLACON",
       description:
-        'Rolacon, is an audit firm in nepal, delivering accounting and finance needs for your business including bookkeeping, business and financial planning, auditing and tax consulting, company registration. ',
-      link: 'http://www.rolacon.com',
+        "Rolacon, is an audit firm in nepal, delivering accounting and finance needs for your business including bookkeeping, business and financial planning, auditing and tax consulting, company registration. ",
+      link: "http://www.rolacon.com",
     },
     {
-      projectName: 'Travelix',
+      projectName: "Goaly",
       description:
-        'Lorem ipsum dolor sit amet, consecteturadipisicing elit. Ducimus facere dolorum, quo perspiciatis delectus aliquid!',
-      image: 'travelix.png',
-      link: 'https://technext.github.io/travelix/index.html',
+        "A hungarian site for procastinators. Add your goal, Add your paying amount, complete your goal.",
+      image: "ntg.png",
+      link: "https://www.goaly.hu/",
     },
     {
-      projectName: 'Natours',
+      projectName: "Travelix",
       description:
-        'Lorem ipsum dolor sit amet, consecteturadipisicing elit. Ducimus facere dolorum, quo perspiciatis delectus aliquid!',
-      image: 'natours.png',
-      link: 'https://natours.netlify.app/',
+        "Lorem ipsum dolor sit amet, consecteturadipisicing elit. Ducimus facere dolorum, quo perspiciatis delectus aliquid!",
+      image: "travelix.png",
+      link: "https://technext.github.io/travelix/index.html",
     },
     {
-      projectName: 'Receipe Blog',
+      projectName: "Natours",
       description:
-        'Lorem ipsum dolor sit amet, consecteturadipisicing elit. Ducimus facere dolorum, quo perspiciatis delectus aliquid!',
-      image: 'dailyReceipe.png',
-      link: 'https://dailyrecipesblog.com/',
+        "Lorem ipsum dolor sit amet, consecteturadipisicing elit. Ducimus facere dolorum, quo perspiciatis delectus aliquid!",
+      image: "natours.png",
+      link: "https://natours.netlify.app/",
     },
     {
-      projectName: 'Tour Nepal',
+      projectName: "Receipe Blog",
       description:
-        'Lorem ipsum dolor sit amet, consecteturadipisicing elit. Ducimus facere dolorum, quo perspiciatis delectus aliquid!',
-      image: 'tourNepal.png',
-      link: 'https://www.toursnepal.com/',
+        "Lorem ipsum dolor sit amet, consecteturadipisicing elit. Ducimus facere dolorum, quo perspiciatis delectus aliquid!",
+      image: "dailyReceipe.png",
+      link: "https://dailyrecipesblog.com/",
     },
     {
-      projectName: 'Animated Landing',
-      description: 'Simple landing page with some amazing animatons',
-      image: 'miro.png',
-      link: 'https://nifty-borg-8bef88.netlify.app/',
-    },
-    {
-      projectName: 'Landing Page',
-      description: 'Landing Page for a hiking site, ',
-      image: 'landing_page.png',
-      link: 'https://quirky-neumann-4fd4ce.netlify.app/',
-    },
-    {
-      projectName: '3D Shoe',
+      projectName: "Tour Nepal",
       description:
-        'Lorem ipsum dolor sit amet, consecteturadipisicing elit. Ducimus facere dolorum, quo perspiciatis delectus aliquid!',
-      image: '3d_shoe.png',
-      link: 'https://reverent-northcutt-850f52.netlify.app/',
+        "Lorem ipsum dolor sit amet, consecteturadipisicing elit. Ducimus facere dolorum, quo perspiciatis delectus aliquid!",
+      image: "tourNepal.png",
+      link: "https://www.toursnepal.com/",
+    },
+    {
+      projectName: "Animated Landing",
+      description: "Simple landing page with some amazing animatons",
+      image: "miro.png",
+      link: "https://nifty-borg-8bef88.netlify.app/",
+    },
+    {
+      projectName: "Landing Page",
+      description: "Landing Page for a hiking site, ",
+      image: "landing_page.png",
+      link: "https://quirky-neumann-4fd4ce.netlify.app/",
     },
   ]);
   const [minLength, setMinLength] = useState(4);
@@ -88,7 +88,7 @@ function Projects() {
       </div>
       {projectLength === minLength && (
         <button className="projectToggle" onClick={showMore}>
-          {' '}
+          {" "}
           Show More
         </button>
       )}
