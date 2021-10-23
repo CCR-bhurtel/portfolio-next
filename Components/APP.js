2b76e4e3f3ff611034cca79adcbf11ffd2b5e84a
cceer <sisir.sital@gmail.com>
2021-10-23 23:06:42 +0545

    done

--- Components/APP.js
+++ Components/APP.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import About from './About';
+import React from "react";
+import About from "./About";
 
-import { toast } from 'react-toastify';
+import { toast } from "react-toastify";
 
-import Contact from './Contact';
-import Counter from './Counter';
-import Footer from './Footer';
-import Header from './Header';
-import Projects from './Projects';
-import Specializations from './Specializations';
-import loading from '../images/loading.gif';
-import Head from 'next/head';
-import Image from 'next/image';
+import Contact from "./Contact";
+import Counter from "./Counter";
+import Footer from "./Footer";
+import Header from "./Header";
+import Projects from "./Projects";
+import Specializations from "./Specializations";
+import loading from "../images/loading.gif";
+import Head from "next/head";
+import Image from "next/image";
 
 toast.configure();
 
@@ -28,15 +28,15 @@ class App extends React.Component {
     try {
       const toggleActiveState = (e) => {
         document
-          .querySelectorAll('.nav-link')
-          .forEach((el) => el.classList.remove('active'));
+          .querySelectorAll(".nav-link")
+          .forEach((el) => el.classList.remove("active"));
 
-        e.classList.add('active');
-        document.querySelector('.nav-collection').classList.remove('active');
-        document.querySelector('body').style.overflow = 'scroll';
+        e.classList.add("active");
+        document.querySelector(".nav-collection").classList.remove("active");
+        document.querySelector("body").style.overflow = "scroll";
       };
 
-      document.addEventListener('scroll', () => {
+      document.addEventListener("scroll", () => {
         const Y = window.scrollY;
         const W = window.scrollX;
         let thresholdHeight;
@@ -49,34 +49,34 @@ class App extends React.Component {
           thresholdHeight = 2350;
         }
         if (Y > thresholdHeight) {
-          const skillBar = document.querySelectorAll('.skill_level');
+          const skillBar = document.querySelectorAll(".skill_level");
           skillBar.forEach((el) => {
-            if (!el.classList.contains('animation')) {
+            if (!el.classList.contains("animation")) {
               const skillPercentage = el.classList[el.classList.length - 1];
-              el.classList.add('animation');
+              el.classList.add("animation");
               el.classList.add(`animation${skillPercentage}`);
             }
           });
         }
 
-        const home = document.querySelector('#homelink');
-        const about = document.querySelector('#aboutlink');
-        const contact = document.querySelector('#contactlink');
-        const project = document.querySelector('#project');
+        const home = document.querySelector("#homelink");
+        const about = document.querySelector("#aboutlink");
+        const contact = document.querySelector("#contactlink");
+        const project = document.querySelector("#project");
 
-        const e = document.querySelector('nav');
+        const e = document.querySelector("nav");
 
         let toggleStyle = (e) => {
-          e.style.position = 'fixed';
-          e.style.background = '#2a363f';
+          e.style.position = "fixed";
+          e.style.background = "#2a363f";
 
           e.style.zIndex = 100000;
-          e.style.left = '0';
-          e.style.top = '0';
+          e.style.left = "0";
+          e.style.top = "0";
 
-          e.style.marginRight = '20px';
-          e.style.paddingLeft = '20px';
-          e.style.justifyContent = 'space-around';
+          e.style.marginRight = "20px";
+          e.style.paddingLeft = "20px";
+          e.style.justifyContent = "space-around";
         };
 
         if (Y >= 0 && Y < 860) {
@@ -114,7 +114,7 @@ class App extends React.Component {
             <meta name="theme-color" content="#000000" />
             <meta
               name="description"
-              content="Personal portfolio site, Shishir Bhutel (cceer bhurtel) is a mern stack developer at gigatech, Nepal. He is a freelance full stack developer at fiverr,  with good knowledge of Node.js, react.js, express.js, mongodb, sql, microservices etc"
+              content="Personal portfolio site, Shishir Bhurtel (cceer bhurtel) is a mern stack developer at gigatech, Nepal. He is a freelance full stack developer at fiverr,  with good knowledge of Node.js, react.js, express.js, mongodb, sql, microservices etc"
             />
             <meta name="robots" content="noindex, nofollow" />
             <meta name="googlebot" content="noindex, nofollow" />
@@ -134,7 +134,7 @@ class App extends React.Component {
             />
 
             <title>
-              Shishir Bhurel | Full Stack Developer -Web Developer at gigatech
+              Shishir Bhurtel | Full Stack Developer -Web Developer at gigatech
             </title>
           </Head>
           <Header />
