8a1db574539def08bbdddf69f759499d4d318cf4
cceer <sisir.sital@gmail.com>
2023-03-09 14:22:14 +0545

    feat: logo changed

--- Components/Footer.js
+++ Components/Footer.js
@@ -1,54 +1,45 @@
-import React from "react";
-import logo from "../public/images/brand-logo.png";
-import Image from "next/image";
+import React from 'react';
+import logo from '../public/images/logo__white.png';
+import Image from 'next/image';
 
 const Footer = () => {
-  return (
-    <footer className="footer">
-      <div className="container">
-        <div className="footer-content">
-          <div className="footer-content-brand">
-            <a href="/" className="logo">
-              <Image src={logo} alt="logo" className="logo-image" />
-            </a>
-            <p className="paragraph">Caffine to code converter</p>
-          </div>
-          <div className="social-media-wrap">
-            <h4 className="footer-heading">Follow Me</h4>
-            <div className="social-media">
-              <a href="#" className="sa-link">
-                <i className="fab fa-twitter"></i>
-              </a>
-              <a
-                href="https://www.facebook.com/bhurtelbro11/"
-                target="blank"
-                className="sa-link"
-              >
-                <i className="fab fa-facebook-square"></i>
-              </a>
-              <a
-                href="https://github.com/CCR-bhurtel"
-                target="blank"
-                className="sa-link"
-              >
-                <i className="fab fa-github"></i>
-              </a>
-              <a
-                href="https://www.linkedin.com/in/shishir-bhurtel-54974b1b7/"
-                target="blank"
-                className="sa-link"
-              >
-                <i className="fab fa-linkedin"></i>
-              </a>
-              <a href="#" className="sa-link">
-                <i className="fab fa-instagram"></i>
-              </a>
+    return (
+        <footer className="footer">
+            <div className="container">
+                <div className="footer-content">
+                    <div className="footer-content-brand">
+                        <a href="/" className="logo">
+                            <Image src={logo} alt="logo" className="logo-image" />
+                        </a>
+                    </div>
+                    <div className="social-media-wrap">
+                        <h4 className="footer-heading">Follow Me</h4>
+                        <div className="social-media">
+                            <a href="#" className="sa-link">
+                                <i className="fab fa-twitter"></i>
+                            </a>
+                            <a href="https://www.facebook.com/bhurtelbro11/" target="blank" className="sa-link">
+                                <i className="fab fa-facebook-square"></i>
+                            </a>
+                            <a href="https://github.com/CCR-bhurtel" target="blank" className="sa-link">
+                                <i className="fab fa-github"></i>
+                            </a>
+                            <a
+                                href="https://www.linkedin.com/in/shishir-bhurtel-54974b1b7/"
+                                target="blank"
+                                className="sa-link"
+                            >
+                                <i className="fab fa-linkedin"></i>
+                            </a>
+                            <a href="#" className="sa-link">
+                                <i className="fab fa-instagram"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-      </div>
-    </footer>
-  );
+        </footer>
+    );
 };
 
 export default React.memo(Footer);
--- Components/Navbar.js
+++ Components/Navbar.js
@@ -1,131 +1,128 @@
-import { useEffect } from "react";
-import logo from "../public/images/brand-logo.png";
-import React from "react";
-import Image from "next/image";
+import { useEffect } from 'react';
+// import logo from "../public/images/brand-logo.png";
+import logo from '../public/images/logo.png';
+import React from 'react';
+import Image from 'next/image';
 
 function Navbar() {
-  useEffect(() => {}, []);
-  const toggleActiveState = (e) => {
-    // document
-    //   .querySelectorAll(".nav-link")
-    //   .forEach((el) => el.classList.remove("active"));
+    useEffect(() => {}, []);
+    const toggleActiveState = (e) => {
+        // document
+        //   .querySelectorAll(".nav-link")
+        //   .forEach((el) => el.classList.remove("active"));
 
-    // e.classList.add("active");
-    document.querySelector(".nav-collection").classList.remove("active");
-    document.querySelector("body").style.overflow = "scroll";
-  };
-  return (
-    <div id="navcontainer" className="container">
-      <nav>
-        <a href="#" className="brand-logo">
-          <Image src={logo} alt="Logo" />
-        </a>
-        <div className="hamburger-menu">
-          <i
-            style={{ marginRight: "1rem" }}
-            onClick={(e) => {
-              document.querySelector(".nav-collection").classList.add("active");
-              document.querySelector("body").style.overflow = "hidden";
-            }}
-            className="fas fa-bars"
-          >
-            {/* <div className="bar"></div> */}
-          </i>
-        </div>
-        <ul className="nav-collection">
-          <i
-            onClick={(e) => {
-              document
-                .querySelector(".nav-collection")
-                .classList.remove("active");
-              document.querySelector("body").style.overflow = "scroll";
-            }}
-            className="fas fa-times"
-          >
-            {/* <div style={{ transform: 'rotate(45deg)' }} className="bar"></div>
+        // e.classList.add("active");
+        document.querySelector('.nav-collection').classList.remove('active');
+        document.querySelector('body').style.overflow = 'scroll';
+    };
+    return (
+        <div id="navcontainer" className="container">
+            <nav>
+                <a href="#" className="brand-logo">
+                    <Image src={logo} alt="Logo" />
+                </a>
+                <div className="hamburger-menu">
+                    <i
+                        style={{ marginRight: '1rem' }}
+                        onClick={(e) => {
+                            document.querySelector('.nav-collection').classList.add('active');
+                            document.querySelector('body').style.overflow = 'hidden';
+                        }}
+                        className="fas fa-bars"
+                    >
+                        {/* <div className="bar"></div> */}
+                    </i>
+                </div>
+                <ul className="nav-collection">
+                    <i
+                        onClick={(e) => {
+                            document.querySelector('.nav-collection').classList.remove('active');
+                            document.querySelector('body').style.overflow = 'scroll';
+                        }}
+                        className="fas fa-times"
+                    >
+                        {/* <div style={{ transform: 'rotate(45deg)' }} className="bar"></div>
             <div
               style={{ transform: 'rotate(-45deg)', marginTop: '-2px' }}
               className="bar"
             ></div> */}
-          </i>
+                    </i>
 
-          <li className="nav-item active">
-            <a
-              id="homelink"
-              onClick={(e) => {
-                toggleActiveState(e.target);
-              }}
-              href="#header"
-              className="nav-link"
-            >
-              Home
-            </a>
-            <div></div>
-          </li>
-          <li className="nav-item">
-            <a
-              id="aboutlink"
-              onClick={(e) => {
-                toggleActiveState(e.target);
-              }}
-              href="#about"
-              className="nav-link"
-            >
-              About
-            </a>
-            <div></div>
-          </li>
-          <li className="nav-item">
-            <a
-              id="projectlink"
-              onClick={(e) => {
-                toggleActiveState(e.target);
-              }}
-              href="#projects"
-              className="nav-link"
-            >
-              Projects
-            </a>
-            <div></div>
-          </li>
-          <li className="nav-item">
-            <a
-              id="contactlink"
-              onClick={(e) => {
-                toggleActiveState(e.target);
-              }}
-              href="#contact"
-              className="nav-link"
-            >
-              Contact
-            </a>
-            <div></div>
-          </li>
-          <li className="nav-item hire-link">
-            <div>
-              <a
-                className="nav-link"
-                href="https://www.fiverr.com/shishrbhurtel1?up_rollout=true"
-                target="blank"
-              >
-                {" "}
-                Hire me
-              </a>
-            </div>
-          </li>
-        </ul>
-        <div
-          onClick={(e) => {
-            document
-              .querySelector(".nav-collection")
-              .classList.remove("active");
-            document.querySelector("body").style.overflow = "scroll";
-          }}
-          className="backdrop"
-        ></div>
-      </nav>
-    </div>
-  );
+                    <li className="nav-item active">
+                        <a
+                            id="homelink"
+                            onClick={(e) => {
+                                toggleActiveState(e.target);
+                            }}
+                            href="#header"
+                            className="nav-link"
+                        >
+                            Home
+                        </a>
+                        <div></div>
+                    </li>
+                    <li className="nav-item">
+                        <a
+                            id="aboutlink"
+                            onClick={(e) => {
+                                toggleActiveState(e.target);
+                            }}
+                            href="#about"
+                            className="nav-link"
+                        >
+                            About
+                        </a>
+                        <div></div>
+                    </li>
+                    <li className="nav-item">
+                        <a
+                            id="projectlink"
+                            onClick={(e) => {
+                                toggleActiveState(e.target);
+                            }}
+                            href="#projects"
+                            className="nav-link"
+                        >
+                            Projects
+                        </a>
+                        <div></div>
+                    </li>
+                    <li className="nav-item">
+                        <a
+                            id="contactlink"
+                            onClick={(e) => {
+                                toggleActiveState(e.target);
+                            }}
+                            href="#contact"
+                            className="nav-link"
+                        >
+                            Contact
+                        </a>
+                        <div></div>
+                    </li>
+                    <li className="nav-item hire-link">
+                        <div>
+                            <a
+                                className="nav-link"
+                                href="https://www.fiverr.com/shishrbhurtel1?up_rollout=true"
+                                target="blank"
+                            >
+                                {' '}
+                                Hire me
+                            </a>
+                        </div>
+                    </li>
+                </ul>
+                <div
+                    onClick={(e) => {
+                        document.querySelector('.nav-collection').classList.remove('active');
+                        document.querySelector('body').style.overflow = 'scroll';
+                    }}
+                    className="backdrop"
+                ></div>
+            </nav>
+        </div>
+    );
 }
 
 export default React.memo(Navbar);
