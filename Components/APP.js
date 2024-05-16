f3f80030d8a06ec97f41d818f159a07f50afccb1
CCR-bhurtel <bhurtelshishir@gmail.com>
2024-05-16 14:15:49 +0545

    update: new project added

--- Components/APP.js
+++ Components/APP.js
@@ -1,129 +1,128 @@
-import { React, Component } from 'react';
-import About from './About';
+import { React, Component } from "react";
+import About from "./About";
 
-import { toast } from 'react-toastify';
+import { toast } from "react-toastify";
 
-import Contact from './Contact';
-import Counter from './Counter';
-import Footer from './Footer';
-import Header from './Header';
-import Projects from './Projects';
-import Specializations from './Specializations';
-import loading from '../public/images/loading.gif';
-import Head from 'next/head';
-import Image from 'next/image';
+import Contact from "./Contact";
+import Counter from "./Counter";
+import Footer from "./Footer";
+import Header from "./Header";
+import Projects from "./Projects";
+import Specializations from "./Specializations";
+import loading from "../public/images/loading.gif";
+import Image from "next/image";
 
 toast.configure();
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            loading: true,
-        };
-    }
-    componentDidMount() {
-        this.setState({ loading: false });
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true,
+    };
+  }
+  componentDidMount() {
+    this.setState({ loading: false });
+  }
 
-    render() {
-        try {
-            const reveal = () => {
-                const reveals = document.querySelectorAll('.reveal');
-                reveals.forEach((reveal) => {
-                    var windowHeight = window.innerHeight;
-                    var revealTop = reveal.getBoundingClientRect().top;
-                    var revealPoint = 150;
-                    if (revealTop < windowHeight - revealPoint) {
-                        reveal.classList.add('active');
-                    }
-                });
-            };
+  render() {
+    try {
+      const reveal = () => {
+        const reveals = document.querySelectorAll(".reveal");
+        reveals.forEach((reveal) => {
+          var windowHeight = window.innerHeight;
+          var revealTop = reveal.getBoundingClientRect().top;
+          var revealPoint = 150;
+          if (revealTop < windowHeight - revealPoint) {
+            reveal.classList.add("active");
+          }
+        });
+      };
 
-            function initSkill() {
-                const skillBar = document.querySelectorAll('.skill_level');
-                const aboutSection = document.querySelector('#about');
+      function initSkill() {
+        const skillBar = document.querySelectorAll(".skill_level");
+        const aboutSection = document.querySelector("#about");
 
-                var windowHeight = window.innerHeight;
-                var revealTop = aboutSection.getBoundingClientRect().top;
-                var revealPoint = 150;
+        var windowHeight = window.innerHeight;
+        var revealTop = aboutSection.getBoundingClientRect().top;
+        var revealPoint = 150;
 
-                if (revealTop < windowHeight - revealPoint) {
-                    skillBar.forEach((el) => {
-                        if (!el.classList.contains('animation')) {
-                            const skillPercentage = el.classList[el.classList.length - 1];
-                            el.classList.add('animation');
-                            el.classList.add(`animation${skillPercentage}`);
-                        }
-                    });
-                }
+        if (revealTop < windowHeight - revealPoint) {
+          skillBar.forEach((el) => {
+            if (!el.classList.contains("animation")) {
+              const skillPercentage = el.classList[el.classList.length - 1];
+              el.classList.add("animation");
+              el.classList.add(`animation${skillPercentage}`);
             }
-
-            let toggleStyle = (e) => {
-                e.style.position = 'fixed';
-                e.style.background = '#2a363f';
-
-                e.style.zIndex = 100000;
-                e.style.left = '0';
-                e.style.top = '0';
-
-                e.style.marginRight = '20px';
-                e.style.paddingLeft = '20px';
-                e.style.justifyContent = 'space-around';
-            };
-
-            document.addEventListener('scroll', () => {
-                initSkill();
-                reveal();
-
-                const Y = window.scrollY;
-                const W = window.scrollX;
-                let thresholdHeight;
-
-                if (W <= 1440) {
-                    thresholdHeight = 1000;
-                } else if (W < 1024) {
-                    thresholdHeight = 1700;
-                } else if (W < 425) {
-                    thresholdHeight = 2100;
-                }
-                if (Y > thresholdHeight) {
-                }
-
-                const e = document.querySelector('nav');
-
-                if (Y >= 840) {
-                    toggleStyle(e);
-                } else {
-                    e.style = null;
-                }
-            });
-        } catch (err) {
-            console.log(err);
+          });
         }
-        return (
-            <>
-                <div className="content">
-                    {this.state.loading ? (
-                        <div className="loading_screen">
-                            <Image src={loading} alt="Loading" />
-                        </div>
-                    ) : (
-                        <div className="main_contents">
-                            <Header />
-                            <Specializations />
-                            <About />
-                            {/* <Skills /> */}
-                            <Projects />
-                            <Counter />
-                            <Contact />
-                            <Footer />
-                        </div>
-                    )}
-                </div>
-            </>
-        );
+      }
+
+      let toggleStyle = (e) => {
+        e.style.position = "fixed";
+        e.style.background = "#2a363f";
+
+        e.style.zIndex = 100000;
+        e.style.left = "0";
+        e.style.top = "0";
+
+        e.style.marginRight = "20px";
+        e.style.paddingLeft = "20px";
+        e.style.justifyContent = "space-around";
+      };
+
+      document.addEventListener("scroll", () => {
+        initSkill();
+        reveal();
+
+        const Y = window.scrollY;
+        const W = window.scrollX;
+        let thresholdHeight;
+
+        if (W <= 1440) {
+          thresholdHeight = 1000;
+        } else if (W < 1024) {
+          thresholdHeight = 1700;
+        } else if (W < 425) {
+          thresholdHeight = 2100;
+        }
+        if (Y > thresholdHeight) {
+        }
+
+        const e = document.querySelector("nav");
+
+        if (Y >= 840) {
+          toggleStyle(e);
+        } else {
+          e.style = null;
+        }
+      });
+    } catch (err) {
+      console.log(err);
     }
+    return (
+      <>
+        <div className="content">
+          {this.state.loading ? (
+            <div className="loading_screen">
+              <Image src={loading} alt="Loading" />
+            </div>
+          ) : (
+            <div className="main_contents">
+              <Header />
+              <Specializations />
+              <About />
+              {/* <Skills /> */}
+              <Projects />
+              <Counter />
+              <Contact />
+              <Footer />
+            </div>
+          )}
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
--- Components/Projects.js
+++ Components/Projects.js
@@ -1,124 +1,134 @@
-import React, { useEffect, useState } from 'react';
-import Card from './Card';
-import GlassCard from './GlassCard';
+import React, { useEffect, useState } from "react";
+import Card from "./Card";
+import GlassCard from "./GlassCard";
 
 function Projects(props) {
-    const [projects, setProjects] = useState([
-        {
-            projectName: 'Fanorona Game',
-            description:
-                'Fanorona is the  multiplayer strategy game originated from Madagascar. The online version of the game is built in React.js, Redux.js, node.js and socket.io.',
-            image: 'fanorona.png',
-            link: 'https://fanorona-game.netlify.app',
-        },
-        {
-            projectName: 'Electrostatics',
-            description:
-                'A simple elctrostatics model with various modules such as induction and conduction, circular motion of charge, dipole moment etc. It is built in p5.js, vanilla javascript, html and css.',
-            image: 'fanorona.png',
-            link: 'https://electrostatics.netlify.app/',
-        },
+  const [projects, setProjects] = useState([
+    {
+      projectName: "ChatUp",
+      description:
+        "Instant messaging made seamless. Send messages, share media, and connect in real-time.",
+      link: "https://chat-up-phi.vercel.app",
+    },
+    {
+      projectName: "Fanorona Game",
+      description:
+        "Fanorona is the  multiplayer strategy game originated from Madagascar. The online version of the game is built in React.js, Redux.js, node.js and socket.io.",
+      image: "fanorona.png",
+      link: "https://fanorona-game.netlify.app",
+    },
+    {
+      projectName: "Electrostatics",
+      description:
+        "A simple elctrostatics model with various modules such as induction and conduction, circular motion of charge, dipole moment etc. It is built in p5.js, vanilla javascript, html and css.",
+      image: "fanorona.png",
+      link: "https://electrostatics.netlify.app/",
+    },
 
-        {
-            projectName: 'Notion Template Store',
-            description: 'Get different ready made notion templates for different tasks in economic price. ',
-            link: 'https://unlockplaybook.com/',
-        },
-        {
-            projectName: "Scramblen't",
-            description: 'Provide scrambled letters and get words from them.',
-            link: 'https://scramblent.vercel.app/',
-        },
-        {
-            projectName: 'Dividend Tracker',
-            description:
-                'Add, buy and watch stocks. Claim free stocks available in the market. Manage your stocks more efficiently. ',
-            link: 'https://mdt-app.vercel.app/',
-        },
-        {
-            projectName: 'Tic Tac Toe Game',
-            description: 'Currently offline (multiplayer on development) tic tac toe game build on next.js.',
-            link: 'https://tictactoe-ebon-ten.vercel.app/',
-        },
-        {
-            projectName: 'ROLACON',
-            description:
-                'Rolacon, is an audit firm in nepal, delivering accounting and finance needs for your business including bookkeeping, business and financial planning, auditing and tax consulting, company registration. ',
-            link: 'http://www.rolacon.com',
-        },
-       
-        {
-            projectName: 'One Page',
-            description: 'Shopify templete theme for sale.',
-            link: 'https://onepage-eosin.vercel.app/',
-        },
-        {
-            projectName: 'Autonomous',
-            description: 'The automated newsletter site built with next.js',
-            link: 'https://autonomous-three.vercel.app/',
-        },
-        {
-            projectName: 'Paaila Nepal Surgical',
-            description: 'Landing page of surgical equipments dealer.',
-            link: 'https://paaila-nepal.vercel.app/',
-        },
-     
-        {
-            projectName: 'Find Presentation',
-            description:
-                'Lorem ipsum dolor sit amet, consecteturadipisicing elit. Ducimus facere dolorum, quo perspiciatis delectus aliquid!',
-            image: 'tourNepal.png',
-            link: 'https://poetic-mermaid-76ff0a.netlify.app/',
-        },
+    {
+      projectName: "Notion Template Store",
+      description:
+        "Get different ready made notion templates for different tasks in economic price. ",
+      link: "https://unlockplaybook.com/",
+    },
+    {
+      projectName: "Scramblen't",
+      description: "Provide scrambled letters and get words from them.",
+      link: "https://scramblent.vercel.app/",
+    },
+    {
+      projectName: "Dividend Tracker",
+      description:
+        "Add, buy and watch stocks. Claim free stocks available in the market. Manage your stocks more efficiently. ",
+      link: "https://mdt-app.vercel.app/",
+    },
+    {
+      projectName: "Tic Tac Toe Game",
+      description:
+        "Currently offline (multiplayer on development) tic tac toe game build on next.js.",
+      link: "https://tictactoe-ebon-ten.vercel.app/",
+    },
+    {
+      projectName: "ROLACON",
+      description:
+        "Rolacon, is an audit firm in nepal, delivering accounting and finance needs for your business including bookkeeping, business and financial planning, auditing and tax consulting, company registration. ",
+      link: "http://www.rolacon.com",
+    },
 
-        {
-            projectName: 'Travelix',
-            description:
-                'Lorem ipsum dolor sit amet, consecteturadipisicing elit. Ducimus facere dolorum, quo perspiciatis delectus aliquid!',
-            image: 'travelix.png',
-            link: 'https://themewagon.github.io/travelix/',
-        },
+    {
+      projectName: "One Page",
+      description: "Shopify templete theme for sale.",
+      link: "https://onepage-eosin.vercel.app/",
+    },
+    {
+      projectName: "Autonomous",
+      description: "The automated newsletter site built with next.js",
+      link: "https://autonomous-three.vercel.app/",
+    },
+    {
+      projectName: "Paaila Nepal Surgical",
+      description: "Landing page of surgical equipments dealer.",
+      link: "https://paaila-nepal.vercel.app/",
+    },
 
-        {
-            projectName: 'Animated Landing',
-            description: 'Simple landing page with some amazing animatons',
-            image: 'miro.png',
-            link: 'https://nifty-borg-8bef88.netlify.app/',
-        },
-    ]);
-    const [minLength, setMinLength] = useState(4);
+    {
+      projectName: "Find Presentation",
+      description:
+        "Lorem ipsum dolor sit amet, consecteturadipisicing elit. Ducimus facere dolorum, quo perspiciatis delectus aliquid!",
+      image: "tourNepal.png",
+      link: "https://poetic-mermaid-76ff0a.netlify.app/",
+    },
 
-    const [projectLength, setProjectLength] = useState(minLength);
-    const newProjects = projects.slice(0, projectLength);
+    {
+      projectName: "Travelix",
+      description:
+        "Lorem ipsum dolor sit amet, consecteturadipisicing elit. Ducimus facere dolorum, quo perspiciatis delectus aliquid!",
+      image: "travelix.png",
+      link: "https://themewagon.github.io/travelix/",
+    },
 
-    const showMore = () => setProjectLength(projects.length);
-    const showLess = () => setProjectLength(minLength);
+    {
+      projectName: "Animated Landing",
+      description: "Simple landing page with some amazing animatons",
+      image: "miro.png",
+      link: "https://nifty-borg-8bef88.netlify.app/",
+    },
+  ]);
+  const [minLength, setMinLength] = useState(4);
 
-    return (
-        <div className="projects toggleOption projectwala" id="projects">
-            <div className="project-title">
-                <h3 className="project-head">Projects</h3>
-                <div className="project-bar"></div>
-            </div>
+  const [projectLength, setProjectLength] = useState(minLength);
+  const newProjects = projects.slice(0, projectLength);
 
-            <div className="pcontainer reveal">
-                {newProjects.map((project, index) => {
-                    return <GlassCard key={index} projectDetails={{ ...project, index }} />;
-                })}
-            </div>
-            {projectLength === minLength && (
-                <button className="projectToggle" onClick={showMore}>
-                    {' '}
-                    Show More
-                </button>
-            )}
-            {projectLength > minLength && (
-                <button className="projectToggle" onClick={showLess}>
-                    Show Less
-                </button>
-            )}
-        </div>
-    );
+  const showMore = () => setProjectLength(projects.length);
+  const showLess = () => setProjectLength(minLength);
+
+  return (
+    <div className="projects toggleOption projectwala" id="projects">
+      <div className="project-title">
+        <h3 className="project-head">Projects</h3>
+        <div className="project-bar"></div>
+      </div>
+
+      <div className="pcontainer reveal">
+        {newProjects.map((project, index) => {
+          return (
+            <GlassCard key={index} projectDetails={{ ...project, index }} />
+          );
+        })}
+      </div>
+      {projectLength === minLength && (
+        <button className="projectToggle" onClick={showMore}>
+          {" "}
+          Show More
+        </button>
+      )}
+      {projectLength > minLength && (
+        <button className="projectToggle" onClick={showLess}>
+          Show Less
+        </button>
+      )}
+    </div>
+  );
 }
 
 export default React.memo(Projects);
